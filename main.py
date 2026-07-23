c4e769f93513eef0d546ea854364a39d34b104df
Harshit <harshitsinghtil@gmail.com>
2026-07-23 09:18:51 +0530

    Create API routes for path and query parameters

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {"message":"Hello"}
+
+@app.get("/about")
+def about():
+    return {
+        "project":"AI Engineering Roadmap",
+        "author":"Harshit"
+    }
+
+@app.get("/battery/{battery_id}")
+def get_battery(battery_id:int):
+    return {'battery_id':battery_id}
+
+# query 
+@app.get("/students")
+def get_roll_marks(roll_no:int,marks:int):
+    return {
+        'roll_no':roll_no,
+        "marks":marks
+    }
